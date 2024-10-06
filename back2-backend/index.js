991d705758382f341a77203d701974933ad885d5
yooncheawon <qsjh0325@naver.com>
2024-10-06 23:18:33 +0900

    간단한 날씨 화면 서버 테스트

--- back2-backend/index.js
+++ back2-backend/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const weatherRoutes = require('./controllers/routes/weatherRoutes'); // 경로 수정
+
 const app = express();
 const PORT = process.env.PORT || 5173;
 
-// Middleware to parse JSON
+// JSON 파싱 미들웨어
 app.use(express.json());
 
-// Example route for testing
+app.use('/weather', weatherRoutes);
+
+// 기본 테스트 경로 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// Start the server
+// 서버 시작
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
