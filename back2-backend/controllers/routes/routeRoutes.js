f791f077abcc6a4a03ff3ba7270094e50d1c57c5
yooncheawon <qsjh0325@naver.com>
2024-11-11 01:01:02 +0900

    수정

--- back2-backend/controllers/routes/routeRoutes.js
+++ back2-backend/controllers/routes/routeRoutes.js
@@ -1,18 +1,8 @@
-// routeRoutes.js
 const express = require('express');
-const { getSimpleWeather, getDetailedWeather } = require('../controllers/weatherController');
 const router = express.Router();
+const { getRouteInfo } = require('../controllers/routeController');
 
-// 간단한 날씨 정보 (초기 화면)
-router.get('/simple/:location', (req, res, next) => {
-    console.log(`Received request for /simple/${req.params.location}`);
-    next();
-}, getSimpleWeather);
-
-// 상세 날씨 정보 (날씨 상세 보기 버튼 클릭 시)
-router.get('/details/:location', (req, res, next) => {
-    console.log(`Received request for /details/${req.params.location}`);
-    next();
-}, getDetailedWeather);
+// POST 요청 처리
+router.post('/route-info', getRouteInfo);
 
 module.exports = router;
